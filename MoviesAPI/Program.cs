d255f05ff5e8b84472d8e34ec39efe65e901d733
Bhuvana <bhuvana.maganti@myob.com>
2022-02-23 10:21:50 +1100

    Changed the host to db (service defined in the docker-compose) - Working

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -12,7 +12,7 @@ namespace MoviesAPI
             httpListener.Prefixes.Add("http://*:5001/");
             // MoviesRepository moviesRepository = new MoviesRepository();
             // var moviesRepository = new DapperService("Data Source=Movies.SQLite");
-             var moviesRepository = new PostgreSqlDapperService("User ID=postgres;Password=postgres;Host=127.0.0.1;Port=5432;Database=postgres;");
+             var moviesRepository = new PostgreSqlDapperService("User ID=postgres;Password=postgres;Host=db;Port=5432;Database=postgres;");
             Server.Server server = new Server.Server(httpListener, moviesRepository);
             server.Start();
         }
