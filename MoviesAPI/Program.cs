df16f65dbd507ce98734a808c07c6d237d1bbc11
Bhuvana <bhuvana.maganti@myob.com>
2022-02-22 14:08:06 +1100

    With Postgres - host in connection string not working

--- MoviesAPI/Program.cs
+++ MoviesAPI/Program.cs
@@ -12,7 +12,7 @@ namespace MoviesAPI
             httpListener.Prefixes.Add("http://*:5001/");
             // MoviesRepository moviesRepository = new MoviesRepository();
             // var moviesRepository = new DapperService("Data Source=Movies.SQLite");
-             var moviesRepository = new PostgreSqlDapperService("User ID=ieqkzofv;Password=FpuZ3H1Ow8vwXRK-jg6l_gvezILgYkI5;Host=rosie.db.elephantsql.com;Port=5432;Database=ieqkzofv;");
+             var moviesRepository = new PostgreSqlDapperService("User ID=postgres;Password=postgres;Host=127.0.0.1;Port=5432;Database=postgres;");
             Server.Server server = new Server.Server(httpListener, moviesRepository);
             server.Start();
         }
